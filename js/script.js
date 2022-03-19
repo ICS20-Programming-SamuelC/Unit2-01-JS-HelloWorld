610957952ae326b8de7a98627d4d33ddbe435c8e
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-03-19 22:38:00 +0000

    Code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Samuel Carlson All rights reserved
+//
+// Created by: Samuel carlson
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'Javascript is great!'
+}
